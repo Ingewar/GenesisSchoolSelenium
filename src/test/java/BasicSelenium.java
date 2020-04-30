4a8fb87328839de302eb66f6dcb2b657aa237dd1
Ingewar <kovtunihor@gmail.com>
2020-04-30 21:01:27 +0300

    Second lesson

--- src/test/java/BasicSelenium.java
+++ src/test/java/BasicSelenium.java
@@ -1,13 +1,34 @@
+import conf.TestManager;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+import pages.BoardsPage;
+import pages.HomePage;
+import pages.LoginPage;
 
-public class BasicSelenium extends TestManager{
+public class BasicSelenium extends TestManager {
 
     @Test
     public void openHomePage(){
-        driver.get("https://trello.com/");
-        String title = driver.getTitle();
-        Assertions.assertEquals(title, "Trello");
+        HomePage homePage = new HomePage(driver);
+        homePage.openPage(homePage.url);
+        String title = homePage.getTitle();
+        Assertions.assertEquals(title, homePage.title);
+    }
+
+    @Test
+    public void loginTest(){
+        //Home Page
+        HomePage homePage = new HomePage(driver);
+        homePage.openPage(homePage.url);
+        homePage.loginLink.click();
+        // Login Page
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.emailField.sendKeys("kovtunihor@gmail.com");
+        loginPage.passwordField.sendKeys("2481632az" + Keys.ENTER);
+        // Boards Page
+        BoardsPage boardsPage = new BoardsPage(driver);
+        Assertions.assertTrue(boardsPage.allBoardsContainer.isExist());
     }
 
 }
